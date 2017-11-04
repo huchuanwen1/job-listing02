23d5a06f8b880a3160115e043f4a306866fb73c1
MAC <mac@MACdeMacBook-Air.local>
2017-11-04 15:52:02 +0800

    implement step 5

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+def admin?
+  email == 'xdite@growth.school'
+end
+def admin?
+  is_admin
+end
+def publish!
+  self.is_hidden = false
+  self.save
+end
+  def hide
+    self.is_hidden = true
+    self.save
+  end
+end
